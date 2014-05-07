5ae5801b3d057b0b6b1053eb88c1d04b3c2c9c6f
wada811 <at.wada811@gmail.com>
2014-05-07 16:14:23 +0900

    prevent from re-creating directories symlink

--- dotfilesInstaller.sh
+++ dotfilesInstaller.sh
@@ -4,7 +4,7 @@ ln -s ~/dotfiles/Settings.terminal ~/Settings.terminal
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.sqliterc ~/.sqliterc
-ln -s ~/dotfiles/.vim ~/.vim
+if [ ! -d ~/.vim ]; then ln -s ~/dotfiles/.vim ~/.vim > /dev/null 2>&1; fi
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.zsh ~/.zsh
+if [ ! -d ~/.zsh ]; then ln -s ~/dotfiles/.zsh ~/.zsh > /dev/null 2>&1; fi
 ln -s ~/dotfiles/.zshrc ~/.zshrc
