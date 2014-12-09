c432f3ec94d0637ebc9241793b68ed76b2ac3934
wada811 <at.wada811@gmail.com>
2014-12-09 17:47:05 +0900

    Add command history search settings

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -8,3 +8,10 @@ HISTSIZE=1000000
 SAVEHIST=1000000
 setopt hist_ignore_dups # ignore duplication command history list
 setopt share_history    # share command history data
+
+# 履歴からコマンド補完
+autoload history-search-end
+zle -N history-beginning-search-backward-end history-search-end
+zle -N history-beginning-search-forward-end history-search-end
+bindkey "^p" history-beginning-search-backward-end
+bindkey "^n" history-beginning-search-forward-end
