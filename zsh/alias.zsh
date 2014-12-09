92536a69c12655eba9216c8752197d880c107a0c
wada811 <at.wada811@gmail.com>
2014-12-09 19:31:09 +0900

    Add ls aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -2,5 +2,10 @@
 # zsh alias #
 #############
 
-# Enable colorized output.
-alias ls="ls -G"
+# -G Enable colorized output.
+# -F Display marks.
+# -l long listing
+# -a list entries starting with .
+alias ls="ls -GF"
+alias ll="ls -l"
+alias la="ll -a"
