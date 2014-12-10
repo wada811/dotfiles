600f9ff5cae0499f5e378789d5421b3169a64437
wada811 <at.wada811@gmail.com>
2014-12-10 11:57:23 +0900

    Add cd options

--- zsh/cd.zsh
+++ zsh/cd.zsh
@@ -1,9 +1,17 @@
 ##########
 # zsh cd #
 ##########
+#
+# zsh: 16. Options
+# 16.2.1 Changing Directories
+# http://zsh.sourceforge.net/Doc/Release/Options.html#Changing-Directories
+#
 
 # auto change directory
 setopt auto_cd
 
-# auto directory pushd that you can get dirs list by cd -[tab]
+# Make cd push the old directory onto the directory stack.
 setopt auto_pushd
+
+# Don't push multiple copies of the same directory onto the directory stack.
+setopt pushd_ignore_dups
