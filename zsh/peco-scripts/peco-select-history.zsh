abaf14e66dd1aee3ab007a6ee1b6a75260f82ae3
wada811 <at.wada811@gmail.com>
2015-08-21 16:03:53 +0900

    Use history reverse option instead of tac

--- zsh/peco-scripts/peco-select-history.zsh
+++ zsh/peco-scripts/peco-select-history.zsh
@@ -1,11 +1,5 @@
 function peco-select-history() {
-    local tac
-    if which tac > /dev/null; then
-        tac="tac"
-    else
-        tac="tail -r"
-    fi
-    BUFFER=$(\history -n 1 | \eval $tac | \awk '!a[$0]++' | \peco --query "$LBUFFER")
+    BUFFER=$(\history -n -r 1 | \awk '!a[$0]++' | \peco --query "$LBUFFER")
     CURSOR=$#BUFFER
     # zle clear-screen
 }
