fa844ebdb10dd68d95e92a296add86920924c81d
wada811 <at.wada811@gmail.com>
2020-09-01 11:26:09 +0900

    peco で gh pr checkout
    GitHub謹製のghコマンドとpecoを組み合わせて、高速にPull Requestのブランチにチェックアウトする。 - 文字っぽいの。 https://fromatom.hatenablog.com/entry/2020/03/31/135410

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -10,3 +10,6 @@ bindkey '^r' peco-select-history
 
 # 一覧からファイルを選択
 bindkey '^f' peco-select-file
+
+# PR 一覧から checkout
+bindkey '^g^p' peco-gh-pr-checkout
---
+++ zsh/peco-scripts/peco-gh-pr-checkout.zsh
@@ -0,0 +1,9 @@
+function peco-gh-pr-checkout() {
+    local selected_pr_id=$(gh pr list | peco | awk '{ print $1 }')
+    if [ -n "$selected_pr_id" ]; then
+        BUFFER="gh pr checkout ${selected_pr_id}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N peco-gh-pr-checkout
